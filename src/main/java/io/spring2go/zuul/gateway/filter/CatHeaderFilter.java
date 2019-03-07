95061109e1cbb2c8eb32a1ac090e58668de01ec4
申 <shenshanghua@ez-hosbed.com>
2019-03-07 18:01:54 +0800

    2019-3-7演示版

---
+++ src/main/java/io/spring2go/zuul/gateway/filter/CatHeaderFilter.java
@@ -0,0 +1,42 @@
+package io.spring2go.zuul.gateway.filter;
+
+import com.dianping.cat.Cat;
+import com.dianping.cat.Cat.Context;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import io.spring2go.zuul.gateway.catherper.CatContext;
+import io.spring2go.zuul.gateway.catherper.CatHttpConstants;
+import org.springframework.stereotype.Component;
+
+// 借助Zuul Filter以跨进程边界方式传递CAT调用链上下文
+@Component
+public class CatHeaderFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+	     // 保存和传递CAT调用链上下文
+	    Context ctx = new CatContext();
+	    Cat.logRemoteCallClient(ctx);
+		RequestContext requestContext = RequestContext.getCurrentContext();
+	    requestContext.addZuulRequestHeader(CatHttpConstants.CAT_HTTP_HEADER_ROOT_MESSAGE_ID, ctx.getProperty(Cat.Context.ROOT));
+	    requestContext.addZuulRequestHeader(CatHttpConstants.CAT_HTTP_HEADER_PARENT_MESSAGE_ID, ctx.getProperty(Cat.Context.PARENT));
+	    requestContext.addZuulRequestHeader(CatHttpConstants.CAT_HTTP_HEADER_CHILD_MESSAGE_ID, ctx.getProperty(Cat.Context.CHILD));
+	    return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 10;
+	}
+
+}
